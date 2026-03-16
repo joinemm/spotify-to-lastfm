ce8f09a855961dde89fe3b0a48738151e9adfddb
Joonas Rautiola <joonas@rautiola.co>
2026-03-16 16:26:45 +0200

    :technologist: Add nix flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "spotify-to-lastfm development shell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              (pkgs.python313.withPackages (ps: [
+                ps.aiohttp
+                ps.python-dotenv
+              ]))
+            ];
+          };
+        });
+    };
+}
